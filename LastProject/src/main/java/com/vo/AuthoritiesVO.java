b1ccd832b50edb7f75c9a309ed710b111b4f5706
KOSMO_13 <KOSMO_13@DESKTOP-5TR91M5>
2020-07-17 22:32:52 +0900

    김나현/setting미완

--- LastProject/src/main/java/com/vo/AuthoritiesVO.java
+++ LastProject/src/main/java/com/vo/AuthoritiesVO.java
@@ -1,29 +1,22 @@
 package com.vo;
 
 public class AuthoritiesVO {
-	private int aId;
+
 	private String userName;
-	private String aName;
+	private String authority;
 	private String delYn;
 	
-		
-	public int getaId() {
-		return aId;
-	}
-	public void setaId(int aId) {
-		this.aId = aId;
-	}
 	public String getUserName() {
 		return userName;
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	public String getaName() {
-		return aName;
+	public String getAuthority() {
+		return authority;
 	}
-	public void setaName(String aName) {
-		this.aName = aName;
+	public void setAuthority(String authority) {
+		this.authority = authority;
 	}
 	public String getDelYn() {
 		return delYn;
