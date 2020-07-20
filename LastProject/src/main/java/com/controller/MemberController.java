51104f8d0ebcd8286364b7fd11fac149136d29ff
KOSMO_13 <KOSMO_13@DESKTOP-5TR91M5>
2020-07-21 02:01:10 +0900

    김나현/start,testController,+MainController,회원가입 미완

--- LastProject/src/main/java/com/controller/MemberController.java
+++ LastProject/src/main/java/com/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +11,17 @@ public class MemberController {
 
 
 	@RequestMapping("/{step}.user")
-	public String page(@PathVariable String step) {
+	public String page(@PathVariable String step,HttpSession session) {
+		
+//		System.out.println("세션값= "+session.getAttribute("mkId"));
 		return "member/"+step;
 	
 	}
 	
 	//회원 가입 버튼 눌렀을 때
 	public String userRegist() {
-		
-		
 		return "";
 	}
+	
+	
 }
